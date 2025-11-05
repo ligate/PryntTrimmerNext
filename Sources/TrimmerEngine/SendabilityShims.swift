21b38479a166cba8740b79243ab1e7bd8192b335
ligate <liggrec@gmail.com>
2025-11-05 11:47:51 +0300

    Add Sendable conformance to AVFoundation types
    Declare AVFoundation types as manually safe to pass across suspension.

--- Sources/TrimmerEngine/SendabilityShims.swift
+++ Sources/TrimmerEngine/SendabilityShims.swift
@@ -1,1 +1,8 @@
+@preconcurrency import AVFoundation
+
+// Declares AVFoundation types as "manually safe to pass" across suspension.
+// We still avoid reading/writing them after await (see exportAsync()).
+extension AVAssetExportSession: @unchecked Sendable {}
+extension AVURLAsset: @unchecked Sendable {}
+// (AVAsset itself isn’t stored/used across await in our code path anymore.)
 
